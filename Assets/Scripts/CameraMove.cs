2460bb94074404ed8e244297d7686fd42a8466a1
CatMao <catmao@CatMaode-MacBook-Pro.local>
2017-08-12 21:09:12 +0800

    Basic architecture.

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+	
+	public float turnSpeed = 50f;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		if(Input.GetKey(KeyCode.UpArrow))
+			transform.Rotate(new Vector3(-turnSpeed * Time.deltaTime, 0f, 0f));
+		if(Input.GetKey(KeyCode.DownArrow))
+			transform.Rotate(new Vector3(turnSpeed * Time.deltaTime, 0f, 0f));
+		if(Input.GetKey(KeyCode.LeftArrow))
+			transform.Rotate(new Vector3(0f, -turnSpeed * Time.deltaTime, 0f));
+		if(Input.GetKey(KeyCode.RightArrow))
+			transform.Rotate(new Vector3(0f, turnSpeed * Time.deltaTime, 0f));
+	}
+}
