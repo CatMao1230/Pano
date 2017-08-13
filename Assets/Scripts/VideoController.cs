de3b35805c5e7a8798ce7754ac38a766dfe93cb3
CatMao <catmao@CatMaode-MacBook-Pro.local>
2017-08-13 20:37:00 +0800

    Can play video.

---
+++ Assets/Scripts/VideoController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoController : MonoBehaviour {
+
+	private MovieTexture movie;
+	// Use this for initialization
+	void Start () {
+		movie = GetComponent<Renderer>().material.mainTexture as MovieTexture;
+		movie.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
