93e12781a4b393d36344e60fa0490457d25bcacb
CatMao <catmao@CatMaode-MacBook-Pro.local>
2017-08-24 22:10:56 +0800

    Add video interaction scene.

---
+++ Assets/Scripts/InteractionVideo.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionVideo : MonoBehaviour {
+
+	public MovieTexture[] videos;
+	private Renderer render;
+	private MovieTexture curVideo;
+
+	void Start () {
+		render = GetComponent<Renderer> ();
+		curVideo = videos[0];
+		render.material.mainTexture = curVideo;
+		videos[0].loop = true;
+	}
+
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) {
+			PlayVideo (videos [1]);
+		} else {
+			PlayVideo (videos [0]);
+		}
+	}
+
+	void PlayVideo (MovieTexture video) {
+		if (video.loop && !curVideo.isPlaying) {
+			curVideo = video;
+			render.material.mainTexture = curVideo;
+			video.Play ();
+		} else {
+			if (!curVideo.isPlaying || !video.isPlaying) {
+				if (!video.loop) {
+					video.Stop ();
+				}
+				curVideo = video;
+				render.material.mainTexture = curVideo;
+				curVideo.Play ();
+			}
+		}
+	}
+
+}
--- Assets/Scripts/VideoController.cs
+++ Assets/Scripts/VideoController.cs
@@ -5,14 +5,13 @@ using UnityEngine;
 public class VideoController : MonoBehaviour {
 
 	private MovieTexture movie;
-	// Use this for initialization
+
 	void Start () {
 		movie = GetComponent<Renderer>().material.mainTexture as MovieTexture;
 		movie.Play();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		
+
 	}
 }
